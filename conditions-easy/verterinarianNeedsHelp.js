d0acf27725308c7c8050bd0ca2e5fc0d78193f94
Andie Swift <andie.swift5@gmail.com>
2020-04-27 20:36:57 -0700

    daily robin challenge

---
+++ conditions-easy/verterinarianNeedsHelp.js
@@ -0,0 +1,12 @@
+/*
+QUESTION:
+
+Your friend is a veterinarian and wants to know how many pet slippers he needs to knit before Christmas.
+Your friend looks after three different species: dogs, cats, and chickens. He asks, very politely, if you can write him a function
+that calculates how many slippers he needs to make and provides you with the subtotal of each species of animal.
+Please write a function for your friend that returns the number of slippers that he will be knitting!
+*/
+
+function howManySlippers (dogs, cats, chickens) {
+  return 4*dogs + 4*cats + 2*chickens;
+}
