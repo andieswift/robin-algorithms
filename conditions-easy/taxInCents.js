4deb9c61222cfb63b312d237a0d6a178d099b2df
Andie Swift <andie.swift5@gmail.com>
2020-05-01 19:27:27 -0700

    completed daily robin challenge

---
+++ conditions-easy/taxInCents.js
@@ -0,0 +1,7 @@
+// Hip hip hooray! It’s tax season! Your CPA has requested that you provide your tax amounts in cents this year.
+// Rather than using pen and paper, wouldn’t it be faster for you to write a
+// function that accepts a dollar amount and a tax percentage and returns the taxed amount in cents ?
+
+function(dollarAmount, taxPercent){
+  return 100*(dollarAmount*taxPercent);
+}
